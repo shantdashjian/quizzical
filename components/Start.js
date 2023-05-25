31f1662e3add0b1dd86f59e0e1852a292145c121
shantdashjian <shantgdashjian@gmail.com>
2023-05-25 09:51:14 +0400

    Modify start text for better UX

--- components/Start.js
+++ components/Start.js
@@ -4,7 +4,7 @@ export default (props) => {
 	return (
 		<div className="container start--container">
 			<h1 className="start--heading">Quizzical</h1>
-			<h2 className="start--description">Test Your Knowledge about Mythology!</h2>
+			<h2 className="start--description">Test Your Knowledge in Mythology!</h2>
 			<button className="start--button" onClick={props.startGame}>Start the Adventure!</button>
 		</div>
 	)
